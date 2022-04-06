efadaa6648bcf3384cb7b9fd7ae46360be119324
Nick Kulway <nkulway@gmail.com>
2022-04-06 19:49:05 -0400

    implemented bcrypt to hash passwords

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api/v1/users', usersRouter);
 
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
+const bcrypt = require('bcrypt')
 var router = express.Router();
+const models = require('../models')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* POST api/v1/users/register */
+router.post('/register', function (req, res, next) {
+  // check if email in use
+  models.User.findAll({
+      where: {
+        username: req.body.username
+      }
+    })
+    .then((users) => {
+      // if it is, send error
+      if (users.length > 0) {
+        res.status(400).json({
+          error: 'username already in use'
+        })
+        return
+      }
+
+      // if not, create new user
+      // hash password
+      bcrypt.hash(req.body.password, 10)
+        .then(hash => {
+          // store in db
+          models.User.create({
+              username: req.body.username,
+              password: hash
+            })
+            .then(user => {
+              // send back new user
+              res.status(201).json(user)
+            })
+        })
+    })
 });
 
 module.exports = router;
