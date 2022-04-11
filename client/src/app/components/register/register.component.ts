0f630405847e890627358931e35e27a68fd83289
Nick Kulway <nkulway@gmail.com>
2022-04-11 00:42:31 -0400

    added registration functionality

---
+++ client/src/app/components/register/register.component.ts
@@ -0,0 +1,42 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { User } from 'src/app/User';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() onHandleUser: EventEmitter<User> = new EventEmitter()
+  id: number
+  username: string
+  password: string
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRegister() {
+    if(!this.username || !this.password) {
+      alert('Please add a valid username and password')
+      return
+    }
+    const userLogin = {
+      id: this.id,
+      username: this.username,
+      password: this.password
+    }
+
+
+    this.onHandleUser.emit(userLogin)
+
+
+
+    this.username = ''
+    this.password = ''
+
+    console.log('works')
+  }
+
+}
