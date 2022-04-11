8fa5224b0d64dae08fcfc504031f0790e312d698
Nick Kulway <nkulway@gmail.com>
2022-04-11 01:39:28 -0400

    debugging heroku

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'client/dist')));
 app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/todos', todosRouter);
-
+app.get('*', (req,res)=> {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'))
+})
 
 module.exports = app;
