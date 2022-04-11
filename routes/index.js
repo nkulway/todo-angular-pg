481095027b44b07ce9d2093c1ddd154b5f8d0b21
Nick Kulway <nkulway@gmail.com>
2022-04-11 11:09:16 -0400

    added http status code to index route

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('index', { title: 'Express' });
+  res.status(200).send('index', { title: 'Express' });
 });
 
 module.exports = router;
