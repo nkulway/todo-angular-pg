db665e16534cf7cc4b26cda6d3fd08a59b62fd4b
Nick Kulway <nkulway@gmail.com>
2022-04-13 10:40:53 -0400

    return config role back to default "postgres"

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/dist/todo-angular-pg')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter);
