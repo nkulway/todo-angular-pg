bd6ffdf1f854ec9f53c2d59ae095bb76fb479d5e
Nick Kulway <nkulway@gmail.com>
2022-04-13 08:05:33 -0400

    debugged server error

--- app.js
+++ app.js
@@ -17,13 +17,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'client/dist')));
+app.use(express.static(path.join(__dirname, 'client/dist/todo-angular-pg')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/users', usersRouter);
 app.use('/api/v1/todos', todosRouter);
 app.get('*', (req,res)=> {
-  res.sendFile(path.join(__dirname, 'client/dist/index.html'))
+  res.sendFile(path.join(__dirname, 'client/dist/todo-angular-pg/index.html'))
 })
 
 app.use((req, res, next) => {
